80dca759a041f6f1433196db73bafc91c230be40
tomoa75 <84170526+tomoa75@users.noreply.github.com>
2026-03-30 13:10:43 +0200

    Validacija tekst postavljen da uvijek zauzima prostor jednako

--- src/ErrorMessage.jsx
+++ src/ErrorMessage.jsx
@@ -1,3 +1,3 @@
-export default function ErrorMessage() {
-  return <p className="errormessage">the field is required</p>;
+export default function ErrorMessage({ className }) {
+  return <p className={`errormessage ${className}`}>the field is required</p>;
 }
